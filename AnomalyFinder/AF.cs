1f4f45fdec817cfb20da2d261939db20b2e0021d
Shane Feek <shane.feek@structuretec.local>
2020-07-28 12:58:09 -0400

    Update to add Moving Z scores

--- AnomalyFinder/AF.cs
+++ AnomalyFinder/AF.cs
@@ -26,6 +26,31 @@ namespace AnomalieFinder
             return srt[size / 2];
         }
 
+        // Calculate average
+        public double Avg(double[] buffer, int size)
+        {
+            int i;
+            double total = 0;
+
+            for (i = 0; i < size; i++)
+                total += buffer[i];
+
+            return total / size;
+        }
+
+        // Calculate Standard Deviation of Population
+        public double SDPop(double[] buffer, int size)
+        {
+            double mean, standardDeviation = 0.0;
+            int i;
+            mean = Avg(buffer, size);
+
+            for (i = 0; i < size; ++i)
+                standardDeviation += Math.Pow(buffer[i] - mean, 2);
+
+            return Math.Sqrt(standardDeviation / size);
+        }
+
         // Calculate Robust Z-Score
         private double[] ZRobust(double[] buffer, int size)
         {
@@ -44,6 +69,30 @@ namespace AnomalieFinder
             for (int i = 0; i < size; i++)
             {
                 output[i] = (0.6745 * (buffer[i] - median1)) / median2;
+            }
+
+            return output;
+        }
+
+        // Calculate Moving Z score
+        private double[] ZMoving(double[] buffer, int size, int w)
+        {
+            double[] output = new double[size];
+            double[] slice = new double[w];
+            int o, j, s;
+            double avg, sd;
+
+            o = 0;
+            for(int i=w; i<size; i++)
+            {
+                output[i] = 0;
+                j = i - w;
+                s = 0;
+                for (int x = j; x < i; x++)
+                    slice[s++] = buffer[x];
+                avg = Avg(slice, w);
+                sd = SDPop(slice, w);
+                output[i] = (buffer[i] - avg) / sd;
             }
 
             return output;
@@ -78,15 +127,21 @@ namespace AnomalieFinder
         private void btnSearch_Click(object sender, EventArgs e)
         {
             double pd;
+            int zsize;
             bool results = false;
+            double[] zvalues;
+            string stype, dtype;
+            string z;
+
 
             try
             {
                 pd = Convert.ToDouble(txtSensitivity.Text);
             }
-            catch {
+            catch
+            {
                 txtResults.AppendText("Z Threshold Error!");
-                return; 
+                return;
             }
 
             if (pd < 0.0)
@@ -104,18 +159,58 @@ namespace AnomalieFinder
                 return;
             }
 
-            double[] zvalues = ZRobust(values, values.Length);
+            if (values.Length < 5)
+            {
+                txtResults.AppendText("Too Few Entries Error!");
+                return;
+            }
 
-            for (int i = 0; i < values.Length; i++)
+            if (chkMovingZ.Checked == true)
+            {
+                try
+                {
+                    zsize = Convert.ToInt32(txtWindowSize.Text);
+                }
+                catch
+                {
+                    txtResults.AppendText("Moving Z Size Error!");
+                    return;
+                }
+
+                if (zsize < 3 || zsize > (values.Length / 3))
+                {
+                    txtResults.AppendText("Moving Z Size Error!");
+                    return;
+                }
+
+                stype = "  rise";
+                dtype = "  fall";
+
+                zvalues = ZMoving(values, values.Length, zsize);
+            }
+            else
+            {
+                stype = "  spike";
+                dtype = "  dip";
+
+                zvalues = ZRobust(values, values.Length);
+            }
+
+            for (int i = 0; i < zvalues.Length; i++)
             {
                 double zv = zvalues[i];
+
+                z = ")  Z = ";
+                if (zv < -100.0) { zv = -100.0; z = ")  Z < "; }
+                if (zv > 100.0) { zv = 100.0; z = ")  Z > "; }
+
                 if (Math.Abs(zv) >= pd)
                 {
                     results = true;
                     if (zv > 0)
-                        txtResults.AppendText("#" + (i + 1).ToString() + " (" + String.Format("{0:.###}", values[i]) + ")  Z = " + String.Format("{0:.0}", zv) + "   Spike" + Environment.NewLine);
+                        txtResults.AppendText("#" + (i + 1).ToString() + " (" + String.Format("{0:.###}", values[i]) + z + String.Format("{0:.0}", zv) + stype + Environment.NewLine);
                     else
-                        txtResults.AppendText("#" + (i + 1).ToString() + " (" + String.Format("{0:.###}", values[i]) + ")  Z = " + String.Format("{0:.0}", zv) + "   Dip" + Environment.NewLine);
+                        txtResults.AppendText("#" + (i + 1).ToString() + " (" + String.Format("{0:.###}", values[i]) + z + String.Format("{0:.0}", zv) + dtype + Environment.NewLine);
                 }
             }
 
